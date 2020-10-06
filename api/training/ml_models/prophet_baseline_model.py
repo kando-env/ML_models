ebc34a2907c5caae8cc9ac8b6f994ac1f1e7881a
Josh Friedlander <josh@kando.eco>
2020-10-06 10:13:56 +0300

    add handling missing data in prophet, style fixes

--- api/training/ml_models/prophet_baseline_model.py
+++ api/training/ml_models/prophet_baseline_model.py
@@ -1,10 +1,10 @@
 import json
 from abc import ABC
-from datetime import date, time, datetime, timedelta
+from datetime import date, datetime, time, timedelta
 
 import pandas as pd
 from fbprophet import Prophet
-from fbprophet.diagnostics import performance_metrics, cross_validation
+from fbprophet.diagnostics import cross_validation, performance_metrics
 from model_template import ModelTemplate
 
 
@@ -29,34 +29,59 @@ class ProphetBaselineTemplate(ModelTemplate, ABC):
         for (node, sensors) in kwargs['items'].items():
             for sensor in sensors:
                 data = self.process_data(node, sensor)
-                self.model = Prophet(yearly_seasonality=False, weekly_seasonality=True, daily_seasonality=True)
-                if kwargs['country'] is not None:
-                    self.model.add_country_holidays(country_name=kwargs['country'])
-                self.model.fit(data)
-                print('finished fitting model')
-                metadata = performance_metrics(cross_validation(self.model, initial='7 days', period='7 days', horizon='12 hours'))
-                for metric in ['mape', 'rmse', 'mae']:
-                    if metric in metadata:
-                        print(f'returning {metric}')
-                        accuracy = metadata[metric].mean()
-                        break
-                pred_params = {'baseline_hours': kwargs.get('baseline_hours', 24 * 7), 'baseline_only': kwargs.get(
-                    'baseline_only', True), 'from_cache': False}
-                pred = self.do_predict({**kwargs, **pred_params})
-                if 'callback' not in kwargs:
-                    kwargs['callback'] = self.client.generate_callback()
-                response = json.dumps({'node': node, 'sensor': sensor, 'accuracy': accuracy, 'response': pred})
-                self.client.notify_model_training_status(response, kwargs['callback'])
+                if data['y'].isna().sum() / len(data) > 0.9:
+                    response = f'More than 90-% missing data for sensor {sensor} at node {node}, no prediction'
+                else:
+                    self.model = Prophet(yearly_seasonality=False,
+                                         weekly_seasonality=True,
+                                         daily_seasonality=True)
+                    if kwargs['country'] is not None:
+                        self.model.add_country_holidays(
+                            country_name=kwargs['country'])
+                    self.model.fit(data)
+                    print('finished fitting model')
+                    metadata = performance_metrics(
+                        cross_validation(self.model,
+                                         initial='7 days',
+                                         period='7 days',
+                                         horizon='12 hours'))
+                    for metric in ['mape', 'rmse', 'mae']:
+                        if metric in metadata:
+                            print(f'returning {metric}')
+                            accuracy = metadata[metric].mean()
+                            break
+                    pred_params = {
+                        'baseline_hours': kwargs.get('baseline_hours', 24 * 7),
+                        'baseline_only': kwargs.get('baseline_only', True),
+                        'from_cache': False
+                    }
+                    pred = self.do_predict({**kwargs, **pred_params})
+                    if 'callback' not in kwargs:
+                        kwargs['callback'] = self.client.generate_callback()
+                    response = json.dumps({
+                        'node': node,
+                        'sensor': sensor,
+                        'accuracy': accuracy,
+                        'response': pred
+                    })
+                self.client.notify_model_training_status(
+                    response, kwargs['callback'])
 
     def get_metadata(self):
-        df_cv = cross_validation(self.model, initial='7 days', period='7 days', horizon='12 hours')
+        df_cv = cross_validation(self.model,
+                                 initial='7 days',
+                                 period='7 days',
+                                 horizon='12 hours')
         metadata = performance_metrics(df_cv)
         return metadata.to_json()
 
     def do_predict(self, context):
         if context.get('from_cache', True):
             return self.pred
-        future = self.model.make_future_dataframe(4 * context['baseline_hours'], '15min', include_history=False)
+        future = self.model.make_future_dataframe(4 *
+                                                  context['baseline_hours'],
+                                                  '15min',
+                                                  include_history=False)
         forecast = self.model.predict(future)
         forecast = forecast.set_index('ds')
         print('finished prediction')
@@ -64,9 +89,14 @@ class ProphetBaselineTemplate(ModelTemplate, ABC):
             return forecast[['yhat']].to_json()
 
         const_2, const_3 = 1.5, 2
-        forecast = forecast.rename(columns={'yhat_upper': 'H1', 'yhat_lower': 'L1'}).copy()
-        forecast['H2'], forecast['H3'] = const_2 * forecast['H1'], const_3 * forecast['H1']
-        forecast['L2'], forecast['L3'] = const_2 * forecast['L1'], const_3 * forecast['L1']
+        forecast = forecast.rename(columns={
+            'yhat_upper': 'H1',
+            'yhat_lower': 'L1'
+        }).copy()
+        forecast['H2'], forecast['H3'] = const_2 * \
+            forecast['H1'], const_3 * forecast['H1']
+        forecast['L2'], forecast['L3'] = const_2 * \
+            forecast['L1'], const_3 * forecast['L1']
         return forecast[['yhat', 'H1', 'H2', 'H3', 'L1', 'L2', 'L3']].to_json()
 
     def process_data(self, point_id, prediction_param):
@@ -75,4 +105,7 @@ class ProphetBaselineTemplate(ModelTemplate, ABC):
         df = pd.DataFrame(data['samplings']).T[[prediction_param]]
         df.index = pd.to_datetime(df.index, unit='s')
         df = df.sort_index().astype(float)
-        return df.reset_index().rename(columns={'index': 'ds', prediction_param: 'y'}).copy()
+        return df.reset_index().rename(columns={
+            'index': 'ds',
+            prediction_param: 'y'
+        }).copy()
