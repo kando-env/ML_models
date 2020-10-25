c61f78c26322d324b6e8fb1a8e3819b3a5c0c4b7
Josh Friedlander <josh@kando.eco>
2020-10-25 12:28:47 +0200

    comment baseline_only, fix missing data handling
    There was a baseline_only option that seems to have been
    triggered automatically, this commit comments it out.
    Also, the missing data handling has been improved.
    If missing data, or all NaNs, the experiment won't crash.

--- api/training/ml_models/prophet_baseline_model.py
+++ api/training/ml_models/prophet_baseline_model.py
@@ -7,6 +7,7 @@ from fbprophet import Prophet
 from fbprophet.diagnostics import cross_validation, performance_metrics
 from model_template import ModelTemplate
 
+
 def get_start_and_end_time():
     end = datetime.combine(date.today(), time.min)
     start = end - timedelta(weeks=16)
@@ -25,49 +26,60 @@ class ProphetBaselineTemplate(ModelTemplate, ABC):
         self.model = None
 
     def do_train(self, **kwargs):
+        # if all data is too sparse (>90% NaN), abort experiment
+        useful_data_found = False
         for (node, sensors) in kwargs['items'].items():
             for sensor in sensors:
                 data = self.process_data(node, sensor)
+                if data is None:
+                    print(
+                        f'No data at all found for sensor {sensor}, no prediction'
+                    )
+                    return
                 if data['y'].isna().sum() / len(data) > 0.9:
-                    response = f'More than 90-% missing data for sensor {sensor} at node {node}, no prediction'
-                    self.experiment_aborted = True
-                else:
-                    self.model = Prophet(yearly_seasonality=False,
-                                         weekly_seasonality=True,
-                                         daily_seasonality=True)
-                    if kwargs['country'] is not None:
-                        self.model.add_country_holidays(
-                            country_name=kwargs['country'])
-                    self.model.fit(data)
-                    print('finished fitting model')
+                    print(
+                        f'More than 90-% missing data for sensor {sensor} at node {node}, no prediction'
+                    )
+                    continue
+                useful_data_found = True  # great! experiment not aborted
+                self.model = Prophet(yearly_seasonality=False,
+                                     weekly_seasonality=True,
+                                     daily_seasonality=True)
+                if kwargs['country'] is not None:
+                    self.model.add_country_holidays(
+                        country_name=kwargs['country'])
+                self.model.fit(data)
+                print('finished fitting model')
 
-                    # metadata = performance_metrics(
-                    #     cross_validation(self.model,
-                    #                      initial='7 days',
-                    #                      period='7 days',
-                    #                      horizon='12 hours'))
-                    # for metric in ['mape', 'rmse', 'mae']:
-                    #     if metric in metadata:
-                    #         print(f'returning {metric}')
-                    #         accuracy = metadata[metric].mean()
-                    #         break
+                # metadata = performance_metrics(
+                #     cross_validation(self.model,
+                #                      initial='7 days',
+                #                      period='7 days',
+                #                      horizon='12 hours'))
+                # for metric in ['mape', 'rmse', 'mae']:
+                #     if metric in metadata:
+                #         print(f'returning {metric}')
+                #         accuracy = metadata[metric].mean()
+                #         break
 
-                    pred_params = {
-                        'baseline_hours': kwargs.get('baseline_hours', 24 * 7),
-                        'baseline_only': kwargs.get('baseline_only', True),
-                        'from_cache': False
-                    }
-                    pred = self.do_predict({**kwargs, **pred_params})
-                    if 'callback' not in kwargs:
-                        kwargs['callback'] = self.client.generate_callback()
-                    response = json.dumps({
-                        'node': node,
-                        'sensor': sensor,
-                        # 'accuracy': accuracy,
-                        'response': pred
-                    })
+                pred_params = {
+                    'baseline_hours': kwargs.get('baseline_hours', 24 * 7),
+                    'baseline_only': kwargs.get('baseline_only', True),
+                    'from_cache': False
+                }
+                pred = self.do_predict({**kwargs, **pred_params})
+                if 'callback' not in kwargs:
+                    kwargs['callback'] = self.client.generate_callback()
+                response = json.dumps({
+                    'node': node,
+                    'sensor': sensor,
+                    # 'accuracy': accuracy,
+                    'response': pred
+                })
                 self.client.notify_model_training_status(
                     response, kwargs['callback'])
+        if not useful_data_found:
+            self.experiment_aborted = True
 
     def get_metadata(self):
         df_cv = cross_validation(self.model,
@@ -87,24 +99,26 @@ class ProphetBaselineTemplate(ModelTemplate, ABC):
         forecast = self.model.predict(future)
         forecast = forecast.set_index('ds')
         print('finished prediction')
-        if context['baseline_only']:
-            return forecast[['yhat']].to_json()
+        # if context['baseline_only']:
+        #     return forecast[['yhat']].to_json()
 
-        const_2, const_3 = 1.5, 2
+        # const_2, const_3 = 1.5, 2
         forecast = forecast.rename(columns={
             'yhat_upper': 'H1',
             'yhat_lower': 'L1'
         }).copy()
         # forecast['H2'], forecast['H3'] = const_2 * \
-            # forecast['H1'], const_3 * forecast['H1']
+        # forecast['H1'], const_3 * forecast['H1']
         # forecast['L2'], forecast['L3'] = const_2 * \
-            # forecast['L1'], const_3 * forecast['L1']
+        # forecast['L1'], const_3 * forecast['L1']
         # return forecast[['yhat', 'H1', 'H2', 'H3', 'L1', 'L2', 'L3']].to_json()
         return forecast[['yhat', 'H1', 'L1']].to_json()
 
     def process_data(self, point_id, prediction_param):
         start, end = get_start_and_end_time()
         data = self.fetch_data(point_id, start, end)
+        if data is None:
+            return data
         df = pd.DataFrame(data['samplings']).T[[prediction_param]]
         df.index = pd.to_datetime(df.index, unit='s')
         df = df.sort_index().astype(float)
