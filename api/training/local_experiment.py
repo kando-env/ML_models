865e8d282538c84385c4c14bb8be1ea9f9b18b58
Josh Friedlander <josh@kando.eco>
2020-12-28 10:55:53 +0200

    move local_experiment file

--- api/training/local_experiment.py
+++ api/training/local_experiment.py
@@ -5,7 +5,7 @@ from api.training.training_experiment import train
 params = {
     'name': 'xgboost_cod',
     'point_id': 1012,
-    "start": datetime(2020, 11, 16, 3, 55).timestamp(),
-    "end": datetime(2020, 12, 16, 3, 55).timestamp()
+    "start": datetime(2020, 11, 10, 3, 55).timestamp(),
+    "end": datetime(2020, 12, 10, 3, 55).timestamp()
 }
 model = train(params)
